9e781b8d1bce3bd028cecba1c1866a3b41c7bfba
Geoffrey Tsai <geoff.tsai@williamhill.com.au>
2017-04-18 10:57:34 +1000

    CICD-1209 #time 4h #comment update to have a defined 404 error

--- config/start.sh
+++ config/start.sh
@@ -2,5 +2,5 @@
 
 consul-template -consul-addr="10.134.16.133:8500" -template "/etc/nginx/conf.d/default.conf.ctpl:/etc/nginx/conf.d/default.conf" -once &
 nginx -c "/etc/nginx/nginx.conf" -g "daemon off;" &
-#consul agent -retry-join-ec2-tag-key="Name" -retry-join-ec2-region="ap-southeast-2" -retry-join-ec2-tag-value="dev-service-discovery" -config-dir="/consul/config" -data-dir="/consul/data" &
+consul agent -retry-join-ec2-tag-key="Name" -retry-join-ec2-region="ap-southeast-2" -retry-join-ec2-tag-value="dev-service-discovery" -config-dir="/consul/config" -data-dir="/consul/data" &
 consul-template -consul-addr="10.134.16.133:8500" -template "/etc/nginx/conf.d/default.conf.ctpl:/etc/nginx/conf.d/default.conf:/bin/bash -c 'nginx -s reload || true'"
