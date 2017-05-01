47e13a831868300aa05548f38dd2f5b0fda10f46
Geoffrey Tsai <geoff.tsai@williamhill.com.au>
2017-05-01 15:24:52 +1000

    catching up

--- config/start.sh
+++ config/start.sh
@@ -1,6 +1,6 @@
 #! /usr/local/bin/dumb-init /bin/sh
 
 consul agent -datacenter="$CONSUL_NAME" -encrypt="3a6nE3qvOSwaPVcg73nxLQ==" -retry-join-ec2-tag-key="Name" -retry-join-ec2-region="ap-southeast-2" -retry-join-ec2-tag-value="$CONSUL_NAME" -config-dir="/consul/config" -data-dir="/consul/data" &
-consul-template -consul-addr="$CONSUL_URI" -template "/etc/nginx/conf.d/default.conf.ctpl:/etc/nginx/conf.d/default.conf" -once &
+consul-template -consul-addr="127.0.0.1:8500" -template "/etc/nginx/conf.d/default.conf.ctpl:/etc/nginx/conf.d/default.conf" -once &
 nginx -c "/etc/nginx/nginx.conf" -g "daemon off;" &
-consul-template -consul-addr="$CONSUL_URI" -template "/etc/nginx/conf.d/default.conf.ctpl:/etc/nginx/conf.d/default.conf:/bin/bash -c 'nginx -s reload || true'"
+consul-template -consul-addr="127.0.0.1:8500" -template "/etc/nginx/conf.d/default.conf.ctpl:/etc/nginx/conf.d/default.conf:/bin/bash -c 'nginx -s reload || true'"
