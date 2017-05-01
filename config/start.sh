e0900ee9da603338b5c876c9f90553625ad462d8
Geoffrey Tsai <geoff.tsai@williamhill.com.au>
2017-05-01 10:50:04 +1000

    update to use the correct location for dumb-init

--- config/start.sh
+++ config/start.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/dumb-init /bin/sh
+#! /usr/local/bin/dumb-init /bin/sh
 
 consul-template -consul-addr="$CONSUL_URI" -template "/etc/nginx/conf.d/default.conf.ctpl:/etc/nginx/conf.d/default.conf" -once &
 nginx -c "/etc/nginx/nginx.conf" -g "daemon off;" &
